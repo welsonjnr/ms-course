22c5c1be01315679929edd162e3038c83e27a5c8
Welson Júnior <welsongt9@live.com>
2022-05-22 18:06:45 -0300

    Project hr-worker created

---
+++ hr-woker/src/main/java/com/msstudy/hrwoker/HrWokerApplication.java
@@ -0,0 +1,13 @@
+package com.msstudy.hrwoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWokerApplication.class, args);
+	}
+
+}
