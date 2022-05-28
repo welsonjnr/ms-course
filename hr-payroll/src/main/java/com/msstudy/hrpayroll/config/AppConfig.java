4196680bf5772f76f2ab2f7da7e4758a18090837
Welson Júnior <welsongt9@live.com>
2022-05-28 18:22:20 -0300

    Rest Teamplate

---
+++ hr-payroll/src/main/java/com/msstudy/hrpayroll/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.msstudy.hrpayroll.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+}
