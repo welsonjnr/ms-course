66fb809358d395d996e672cf568a0e869ff44f08
Welson Júnior <welsongt9@live.com>
2022-05-28 19:06:14 -0300

    Ribbon load balance

--- hr-payroll/src/main/java/com/msstudy/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/msstudy/hrpayroll/HrPayrollApplication.java
@@ -2,8 +2,10 @@ package com.msstudy.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@RibbonClient(name = "hr-worker")
 @SpringBootApplication
 @EnableFeignClients
 public class HrPayrollApplication {
--- hr-woker/src/main/java/com/msstudy/hrwoker/resources/WorkerResource.java
+++ hr-woker/src/main/java/com/msstudy/hrwoker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.msstudy.hrwoker.resources;
 
 import com.msstudy.hrwoker.entities.Worker;
 import com.msstudy.hrwoker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +19,11 @@ import java.util.Optional;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+    private static Logger Logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment env;
+
     @Autowired
     private WorkerRepository repository;
 
@@ -27,6 +35,9 @@ public class WorkerResource {
 
     @GetMapping (value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
+
+        Logger.info("PORT = " + env.getProperty("local.server.port"));
+
         Optional<Worker> worker = repository.findById(id);
         return worker.isPresent() ? ResponseEntity.ok(worker.get()) : ResponseEntity.notFound().build();
     }
