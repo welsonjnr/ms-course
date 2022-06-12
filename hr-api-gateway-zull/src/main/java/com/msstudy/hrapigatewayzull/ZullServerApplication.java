dde71b31a93690e7ff314449f9d395e1b3fc3031
Welson Júnior <welsongt9@live.com>
2022-06-12 18:19:27 -0300

    Created project gateway

---
+++ hr-api-gateway-zull/src/main/java/com/msstudy/hrapigatewayzull/ZullServerApplication.java
@@ -0,0 +1,13 @@
+package com.msstudy.hrapigatewayzull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZullServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZullServerApplication.class, args);
+	}
+
+}
---
+++ hr-api-gateway-zull/src/test/java/com/msstudy/hrapigatewayzull/ZullServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msstudy.hrapigatewayzull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZullServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
